5ad6d2129b041ff8f929e492d50bb15fde287829
lc0301 <38829780+lc0301@users.noreply.github.com>
2019-10-17 20:28:46 +0800

    Add files via upload

---
+++ js/new_file.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#reg").click(function(){
+		window.location = "reg.html";
+	});
+	$("#forgetPassword").click(function(){
+		window.location = "forget_password.html";
+	});
+});
---
+++ js/xx.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+	$("#message-zero").click(function(){
+		window.location = "xxxq.html";
+	});
+	$("#demo1").click(function(){
+		window.location = "zx.html";
+	});
+	$("#demo2").click(function(){
+		window.location = "st.html";
+	});
+	$("#demo3").click(function(){
+		window.location = "zx.html";
+	});
+	$("#demo4").click(function(){
+		window.location = "zx.html";
+	});
+	$("#demo5").click(function(){
+		window.location = "zx.html";
+	});
+	$("#part-one").click(function(){
+		window.location = "xxxq.html";
+	});
+	$("#shouye").click(function(){
+		window.location = "sy.html";
+	});
+	$("#huodong").click(function(){
+		window.location = "huodong.html";
+	});
+	$("#dongtai").click(function(){
+		window.location = "动态.html";
+	});
+	$("#xiaoxi").click(function(){
+		window.location = "消息.html";
+	});
+	$("#wode").click(function(){
+		window.location = "wode.html";
+	});
+	$("#body1").click(function(){
+		window.location = "wdxx.html";
+	});
+	$("#body2").click(function(){
+		window.location = "wdst.html";
+	});
+	$("#body3").click(function(){
+		window.location = "wdgz.html";
+	});
+	$("#body4").click(function(){
+		window.location = "wdhd.html";
+	});
+	$("#body5").click(function(){
+		window.location = "gerenziliao.html";
+	});
+	$("#picture1").click(function(){
+		window.location = "hdxq.html";
+	});
+	$("#reg").click(function(){
+		window.location = "reg.html";
+	});
+	$("#forgetPassword").click(function(){
+		window.location = "forget_password.html";
+	});
+	$("#picture").click(function(){
+		window.location = "jxxq.html";
+	});
+	$("#demo1").click(function(){
+		window.location = "html/zx.html";
+	});
+	$("#demo2").click(function(){
+		window.location = "html/st.html";
+	});
+	$("#part-one").click(function(){
+		window.location = "stxq.html";
+	});
+	
+});
+
